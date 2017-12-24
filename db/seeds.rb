caf5bafabe786ac3deea3f4a7abf05f8ca1126e5
Nazar Kovalchuk <kovalchukny@gmail.com>
2017-12-24 10:47:49 +0200

    Add seeds for PhaseCategory and Phase

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+10.times do |i|
+  category = PhaseCategory.create(name: "Phase Category #{i}")
+  rand(5...10).times { |j| Phase.create(name: "Phase #{i}.#{j}", phase_category: category) }
+end
