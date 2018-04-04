335dcd4a0daebc4c01546c142a2cd347de0bcd82
curious725 <curious725@gmail.com>
2018-04-04 17:42:49 +0300

    rewrite test for with_category_name and add new tests

--- spec/models/phase_spec.rb
+++ spec/models/phase_spec.rb
@@ -3,19 +3,40 @@
 require 'rails_helper'
 
 RSpec.describe Phase, type: :model do
-  it 'with_category_name method should return correct array' do
-    Timecop.freeze(Time.now)
-    phase_categories = create_list(:phase_category_with_phases, 4)
-    expected_array = []
-    phase_categories.each do |phase_category|
-      phase_category.phases.each do |phase|
-        expected_array.push(
-          id: phase.id, name: phase.name, created_at: phase.created_at,
-          category_name: phase_category.name
-        )
-      end
+  describe '.with_category_name' do
+    before(:all) do
+      Timecop.freeze(Time.now)
+      create_list(:phase_category_with_phases, 4)
     end
-    expect(Phase.with_category_name).to match_array(expected_array)
-    Timecop.return
+
+    it 'should return correct array' do
+      expected_array = []
+      Phase.joins(:phase_category).select('phases.id, phases.name,
+        phases.created_at, phase_categories.name as category_name')
+           .map do |phase|
+             expected_array.push(
+               id: phase.id, name: phase.name, created_at: phase.created_at,
+               category_name: phase.category_name
+             )
+           end
+      expect(Phase.with_category_name).to match_array(expected_array)
+    end
+
+    it 'should not return phase_category without phases' do
+      phase_category_without_phases = create(
+        :phase_category, name: 'PhaseCategoryOther'
+      )
+      expect(Phase.with_category_name).not_to include(
+        phase_category_without_phases.as_json
+      )
+    end
+
+    it 'should send 1 sql query to db' do
+      expect { Phase.with_category_name }.not_to exceed_query_limit(1)
+    end
+
+    after(:all) do
+      Timecop.return
+    end
   end
 end
--- test/models/employee_contact_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CandidateContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
